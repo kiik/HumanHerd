9f3c0f4979de4bdd5e49f7b318fc956be2b6e5e3
Meelik Kiik <kiik.meelik@gmail.com>
2016-12-10 23:45:10 +0200

    Add additional assets.

---
+++ Assets/Scripts/ProjectOnGround.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ProjectOnGround : MonoBehaviour
+{
+  private Quaternion savedRotation;
+
+  void OnWillRenderObject()
+  {
+    savedRotation = transform.rotation;
+
+		var eulers = transform.rotation.eulerAngles;
+
+		transform.rotation = Quaternion.Euler(54.0f, eulers.y, eulers.z);
+  }
+
+  //called right after rendering the object
+  void OnRenderObject()
+  {
+    transform.rotation = savedRotation;
+  }
+}
