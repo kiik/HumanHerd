7ed783bfa6ac4532273a41ce5eddc6156d1e42da
jstefan <mikson60@gmail.com>
2016-12-10 15:40:45 +0200

    Added script and prefab folders. Added log fence prefab. Started implementation of player controls. Created PlayerControls scene.

---
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+
+    // Drag
+    public GameObject WallPrefab;
+    Vector2 dragStart;
+    Vector2 dragEnd;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MouseInput();
+        KeyboardInput();
+	}
+
+    void MouseInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MouseRay();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+
+        }
+    }
+
+    void KeyboardInput()
+    {
+
+    }
+
+    void MouseRay()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        Debug.Log(Input.mousePosition);
+    }
+}
