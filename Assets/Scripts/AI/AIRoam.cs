a3cf9d18450f09d6c563199f2a5f80bb3c518564
Meelik Kiik <kiik.meelik@gmail.com>
2016-12-12 15:59:33 +0200

    Update citizen AI

--- Assets/Scripts/AI/AIRoam.cs
+++ Assets/Scripts/AI/AIRoam.cs
@@ -1,34 +1,14 @@
 using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(Rigidbody2D))]
 public class AIRoam : AIBase {
 
-  public float speed = 1.0f;
+  public Bounds roamArea;
 
-  void Start() {
-    navigateTo(new Vector3(3.0f, 0, 0));
+  public void roam() {
+    Vector3 p = new Vector3(Random.Range(roamArea.min.x, roamArea.max.x), Random.Range(roamArea.min.y, roamArea.max.y), 0);
+    navigateTo(p);
   }
 
-  void Update() {
-    runMotionBehaviour();
-
-    //controller.SimpleMove(new Vector3(0.0f, 0.1f, 0) * Time.deltaTime);
-  }
-
-  protected override void onMotionUpdate(Vector3 dir) {
-    dir *= speed;
-    //gameObject.GetComponent<Rigidbody2D>().AddForce(dir * speed);
-
-    //Debug.Log(dir);
-    gameObject.GetComponent<Rigidbody2D>().velocity = dir * speed;
-
-
-    //Debug.Log(dir * speed);
-  }
-
-  void OnTriggerEnter2D(Collider2D o) {
-    //Debug.Log((transform.position - o.gameObject.transform.position));
-  }
 
 }
